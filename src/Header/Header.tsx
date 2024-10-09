6db12d7cfc6dff44be35e2dad0ecefdd6939546d
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-09 23:23:03 +0530

    notif icon

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,16 +1,25 @@
-import { IconAsset, IconBinocularsFilled } from "@tabler/icons-react";
+import { Avatar } from "@mantine/core";
+import { IconAsset, IconBellPlus, IconBinocularsFilled, IconTool } from "@tabler/icons-react";
 
 const Header =()=>{
 return <div className="w-full bg-black px-6 text-white h-28 flex justify-between items-center">
-    <div className="flex gap-3">
-        <IconBinocularsFilled/>
-        Opportune
+    <div className="flex gap-3 items-center">
+        <IconBinocularsFilled className="h-10 w-10" />
+        <div className="text-3xl font-semibold">WorkWatch</div>
     </div>
-    <div>
-        links
+    <div className="flex gap-3 items-center">
+        <a href="/">Find Jobs</a>
+        <a href="/">Find Talent</a>
+        <a href="/">Upload Job</a>
+        <a href="/">About Us</a>
     </div>
-    <div>
-        logo
+    <div className="flex gap-5 items-center">
+    <IconBellPlus/>
+    <div className="flex gap-2 items-center">
+        <div>Anvesh</div>
+        <Avatar src="avatar.png" alt="it's me" />
+    </div>
+    <IconTool/>
     </div>
 </div>
 }
