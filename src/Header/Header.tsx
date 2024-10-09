a7be0aad39694aaf624619f7cb2ce9add1061f9f
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-09 15:07:17 +0530

    setup

---
+++ src/Header/Header.tsx
@@ -0,0 +1,14 @@
+const Header =()=>{
+return <div className="w-full bg-black px-6 text-white h-28 flex justify-between items-center">
+    <div>
+        logo
+    </div>
+    <div>
+        links
+    </div>
+    <div>
+        logo
+    </div>
+</div>
+}
+export default Header;
