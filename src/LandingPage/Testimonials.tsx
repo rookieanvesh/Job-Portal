8e7884d6fce800914cfb1ecf9df9ac07c6c3230d
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-12 23:22:38 +0530

    users say about us

--- src/LandingPage/Testimonials.tsx
+++ src/LandingPage/Testimonials.tsx
@@ -1,4 +1,8 @@
 const Testimonials=()=>{
-    return <div></div>
+    return <div className="mt-20 pb-5">
+    <div className="text-4xl text-center font-semibold mb-3 text-woodsmoke-100">What <span className="text-coral-red-500">users </span>
+    say about us?</div>
+
+    </div>
 }
 export default Testimonials
