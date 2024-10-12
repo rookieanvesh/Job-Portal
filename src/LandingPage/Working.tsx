17fcad2ab31efbf4b046471f79eae21a9c03cbf0
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-12 21:48:17 +0530

    working

--- src/LandingPage/Working.tsx
+++ src/LandingPage/Working.tsx
@@ -1,4 +1,10 @@
 const Working=()=>{
-    return <div></div>
+    return <div className="mt-20 pb-5">
+    <div className="text-4xl text-center font-semibold mb-3 text-woodsmoke-100">How it <span className="text-coral-red-500">Works </span>
+    </div>
+    <div className="text-lg mx-auto mb-10 text-woodsmoke-300 text-center w-1/2">Unlock your potential and find your dream job. 
+    Browse our diverse range of job categories and kick-start your career today!</div>
+
+    </div>
 }
 export default Working;
