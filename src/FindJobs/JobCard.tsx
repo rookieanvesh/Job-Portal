c153a15d5b7ced70fc3ea5938ebfad99037b7361
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-15 21:28:02 +0530

    jobcard

--- src/FindJobs/JobCard.tsx
+++ src/FindJobs/JobCard.tsx
@@ -1,5 +1,21 @@
 const JobCard = () => {
-    return <div></div>
+    return <div>
+        <div>
+            <div>
+                <div>
+                    <img src="/" alt="" />
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div></div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 
 }
 export default JobCard;
